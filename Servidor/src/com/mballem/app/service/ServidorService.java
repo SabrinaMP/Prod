d4eeda617a917ad5329b80542a71a58de8bf1533
Guilherme <guilhermed.reiss@gmail.com>
2017-10-25 21:47:31 -0300

    ServidorService implementação me metodo Run

--- Servidor/src/com/mballem/app/service/ServidorService.java
+++ Servidor/src/com/mballem/app/service/ServidorService.java
@@ -5,6 +5,8 @@
  */
 package com.mballem.app.service;
 
+import com.mballem.app.bean.ChatMenssage;
+import com.mballem.app.bean.ChatMenssage.Action;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -20,29 +22,32 @@ import java.util.logging.Logger;
  * @author Administrador
  */
 public class ServidorService {
+
     private ServerSocket serverSocket;
     private Socket socket;
     private Map<String, ObjectOutputStream> mapOnlines = new HashMap<String, ObjectOutputStream>();
-    
+
     public ServidorService() {
         try {
             serverSocket = new ServerSocket(5555);
-            
-            while(true){
+
+            while (true) {
                 socket = serverSocket.accept();
-                
+
                 new Thread(new ListenerSocket(socket)).start();
             }
-            
+
         } catch (IOException ex) {
             Logger.getLogger(ServidorService.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    private class ListenerSocket implements Runnable{
+
+    private class ListenerSocket implements Runnable {
+
         private ObjectOutputStream output;
         private ObjectInputStream input;
-        
-        public ListenerSocket(Socket socket){
+
+        public ListenerSocket(Socket socket) {
             try {
                 this.output = new ObjectOutputStream(socket.getOutputStream());
                 this.input = new ObjectInputStream(socket.getInputStream());
@@ -50,12 +55,46 @@ public class ServidorService {
                 Logger.getLogger(ServidorService.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
+
         @Override
         public void run() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            ChatMenssage menssage = null;
+            try {
+                while ((menssage = (ChatMenssage) input.readObject()) != null) {
+                    Action action = menssage.getAction();
+
+                    if (action.equals(Action.CONNECT)) {
+                        connect(menssage, output);
+                    } else if (action.equals(Action.DISCONNCT)) {
+
+                    } else if (action.equals(Action.SEND_ONE)) {
+
+                    } else if (action.equals(Action.SEND_ALL)) {
+
+                    } else if (action.equals(Action.USERS_ONLINE)) {
+
+                    }
+
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(ServidorService.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(ServidorService.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
         }
-        
+
     }
-            
+
+    private void connect(ChatMenssage menssage, ObjectOutputStream output) {
+        sendOne(menssage, output);
+    }
+    
+    private void sendOne(ChatMenssage menssage, ObjectOutputStream output){
+        try {
+            output.writeObject(menssage);
+        } catch (IOException ex) {
+            Logger.getLogger(ServidorService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
